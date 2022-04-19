9855756bb93547c570b0d2abbec7b47aa2754835
Jari-Matti Ojala <jaroja@wapice.com>
2022-04-19 12:08:51 +0300

    view activity details ready

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react'
+import axios from 'axios';
+import 'semantic-ui-css/semantic.min.css' //Does not work for some odd reason.
+import { Container, Header, List } from 'semantic-ui-react';
+import { useEffect, useState } from "react";
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, [])
+
+function handleSelectActivity(id: string){
+  setSelectedActivity(activities.find(x => x.id === id))
+}
+
+function handleCancelSelectActivity(){
+  setSelectedActivity(undefined);
+}
+
+  return (
+    <>
+      <NavBar />
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard 
+          activities={activities} 
+          selectedActivity={selectedActivity}
+          selectActivity = {handleSelectActivity}
+          cancelSelectActivity = {handleCancelSelectActivity}
+        />
+      </Container>
+    </>
+  );
+}
+
+export default App;
