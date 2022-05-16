434e9d71f63956f2c8769b470aaa7b70d8208bcd
Jari-Matti Ojala <jaroja@wapice.com>
2022-05-16 16:06:18 +0300

    Added client side login/register

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '../..';
 import { Activity } from '../models/activity';
+import { User, UserFormValues } from '../models/user';
 import { store } from '../stores/store';
 
 const sleep = (delay: number) => {
@@ -11,6 +12,12 @@ const sleep = (delay: number) => {
 }
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
+
+axios.interceptors.request.use(config => {
+    const token = store.commonStore.token;
+    if (token && config.headers) config.headers.Authorization = `Bearer ${token}`
+    return config;
+})
 
 axios.interceptors.response.use(async response => {
 
@@ -55,8 +62,8 @@ const responseBody = <T> (response: AxiosResponse<T>) => response.data
 
 const requests = {
     get: <T> (url: string) => axios.get<T>(url).then(responseBody),
-    post: <T> (url: string, body: {}) => axios.post(url).then(responseBody),
-    put: <T> (url: string,  body: {}) => axios.put(url).then(responseBody),
+    post: <T> (url: string, body: {}) => axios.post(url, body).then(responseBody),
+    put: <T> (url: string,  body: {}) => axios.put(url, body).then(responseBody),
     delete: <T>  (url: string) => axios.delete(url).then(responseBody),
 }
 
@@ -68,8 +75,16 @@ const Activities = {
     delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
+const Account = {
+    current:() => requests.get<User>('/account'),
+    login: (user: UserFormValues) => requests.post('/account/login', user),
+    register: (user: UserFormValues) => requests.post('/account/register', user)
+
+}
+
 const agent = {
-    Activities
+    Activities,
+    Account
 }
 
 export default agent
