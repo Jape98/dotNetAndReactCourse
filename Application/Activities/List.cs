443b3625e64c2f80ca967a29da2d59109f962935
Jari Ojala <e1700833@edu.vamk.fi>
2022-05-30 08:36:46 +0300

    Added paging and infinite scroll

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -12,7 +12,7 @@ namespace Application.Activities
     {
         public class Query : IRequest<Result<PagedList<ActivityDto>>>
         {
-            public PagingParams Params { get; set; }
+            public ActivityParams Params { get; set; }
         }
 
         public class Handler : IRequestHandler<Query, Result<PagedList<ActivityDto>>>
@@ -33,9 +33,20 @@ namespace Application.Activities
             public async Task<Result<PagedList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var query = context.Activities
+                    .Where(d => d.Date >= request.Params.StartDate)
                     .OrderBy(d => d.Date)
                     .ProjectTo<ActivityDto>(mapper.ConfigurationProvider, new {currentUsername = userAccessor.GetUsername()})
                     .AsQueryable();
+
+                if (request.Params.IsGoing && !request.Params.IsHost)
+                {
+                    query = query.Where(x => x.Attendees.Any(a => a.Username == userAccessor.GetUsername()));
+                }
+
+                if (request.Params.IsHost && !request.Params.IsGoing)
+                {
+                    query = query.Where(x => x.HostUsername == userAccessor.GetUsername());
+                }
 
                 return Result<PagedList<ActivityDto>>.Success(
                     await PagedList<ActivityDto>.CreateAsync(query, request.Params.PageNumber, request.Params.PageSize)
--- Application/Core/PagingParams.cs
+++ Application/Core/PagingParams.cs
@@ -5,7 +5,7 @@ namespace Application.Core
         private const int MaxPageSize = 50;
         public int PageNumber { get; set; } = 1;
 
-        private int pageSize = 10;
+        private int pageSize = 5;
         public int PageSize
         {
             get => pageSize;
